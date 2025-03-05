42d439c8ba0e3d10d0e9e19f730be090bbbe6946
aaronaaron12 <jetsam_retrial.4p@icloud.com>
2025-03-05 20:44:00 +0000

    Bloodworth 7-1 through 7-16

--- hw7-16.cpp
+++ hw7-16.cpp
@@ -8,20 +8,20 @@ using namespace std;
 int main()
 {
 	double bill = 0.0;
-	double tip = 0.0;
+	
 
 	cout << "Bill amount: ";
 	cin >> bill;
 
 	cout << fixed;
-	while (double rate = 0.1; rate <= 0.25; rate += 0.05)
-	{
-		tip = bill * rate;
-		cout << setprecision(0);
-		cout << rate * 100 << "% tip: ";
-		cout << setprecision(2);
-		cout << "$" << tip << endl;
+	cout << setprecision(2);
+	cout << "10% tip: $" << bill * 0.10 << endl;
+	cout << "15% tip: $" << bill * 0.15 << endl;
+	cout << "20% tip: $" << bill * 0.20 << endl;
 	
+
+	{
+
 	}   // end for
 	return 0;
 }	//end of main function
